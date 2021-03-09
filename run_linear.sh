c3425a72dd036406caacd4dea0becfb4b26535b2
linusericsson <lericsson22@gmail.com>
2021-03-09 16:53:20 +0000

    script with hyperparams for reproducing linear evaluation

---
+++ run_linear.sh
@@ -0,0 +1,164 @@
+python linear.py -d aircraft -m insdis -c 10000.000252621256
+python linear.py -d aircraft -m moco-v1 -c 5623.413277133687
+python linear.py -d aircraft -m pcl-v1 -c 999.999952502551
+python linear.py -d aircraft -m pirl -c 3162.277748945447
+python linear.py -d aircraft -m pcl-v2 -c 3162.277748945447
+python linear.py -d aircraft -m simclr-v1 --no-norm -c 1778.2794843157246
+python linear.py -d aircraft -m moco-v2 -c 5623.413277133687
+python linear.py -d aircraft -m simclr-v2 --no-norm -c 56234.135072196805
+python linear.py -d aircraft -m sela-v2 -c 5623.413277133687
+python linear.py -d aircraft -m infomin -c 3162.277748945447
+python linear.py -d aircraft -m byol -c 17782.793692727315
+python linear.py -d aircraft -m deepcluster-v2 -c 56234.135072196805
+python linear.py -d aircraft -m swav -c 999.999952502551
+python linear.py -d aircraft -m supervised -c 9.99999985098839
+
+python linear.py -d caltech101 -m insdis -c 316227.7712565657
+python linear.py -d caltech101 -m moco-v1 -c 177827.94843157247
+python linear.py -d caltech101 -m pcl-v1 -c 1000000.0025247572
+python linear.py -d caltech101 -m pirl -c 177827.94843157247
+python linear.py -d caltech101 -m pcl-v2 -c 1000000.0025247572
+python linear.py -d caltech101 -m simclr-v1 --no-norm -c 10000.000252621256
+python linear.py -d caltech101 -m moco-v2 -c 316227.7712565657
+python linear.py -d caltech101 -m simclr-v2 --no-norm -c 31.622778304361788
+python linear.py -d caltech101 -m sela-v2 -c 56234.135072196805
+python linear.py -d caltech101 -m infomin -c 316227.7712565657
+python linear.py -d caltech101 -m byol -c 1.0
+python linear.py -d caltech101 -m deepcluster-v2 -c 100000.00252621254
+python linear.py -d caltech101 -m swav -c 56234.135072196805
+python linear.py -d caltech101 -m supervised -c 0.3162277621819913
+
+python linear.py -d cars -m insdis -c 3162.277748945447
+python linear.py -d cars -m moco-v1 -c 1778.2794843157246
+python linear.py -d cars -m pcl-v1 -c 3162.277748945447
+python linear.py -d cars -m pirl -c 5623.413277133687
+python linear.py -d cars -m pcl-v2 -c 999.999952502551
+python linear.py -d cars -m simclr-v1 --no-norm -c 17.782794106882072
+python linear.py -d cars -m moco-v2 -c 5623.413277133687
+python linear.py -d cars -m simclr-v2 --no-norm -c 56.23413185099295
+python linear.py -d cars -m sela-v2 -c 31.622778304361788
+python linear.py -d cars -m infomin -c 5623.413277133687
+python linear.py -d cars -m byol -c 316.2277690737784
+python linear.py -d cars -m deepcluster-v2 -c 100.00000223517424
+python linear.py -d cars -m swav -c 177.8279410688207
+python linear.py -d cars -m supervised -c 17.782794106882072
+
+python linear.py -d cifar10 -m insdis -c 177827.94843157247
+python linear.py -d cifar10 -m moco-v1 -c 177.8279410688207
+python linear.py -d cifar10 -m pcl-v1 -c 31.622778304361788
+python linear.py -d cifar10 -m pirl -c 31622.77530666721
+python linear.py -d cifar10 -m pcl-v2 -c 9.99999985098839
+python linear.py -d cifar10 -m simclr-v1 --no-norm -c 0.3162277621819913
+python linear.py -d cifar10 -m moco-v2 -c 316227.7712565657
+python linear.py -d cifar10 -m simclr-v2 --no-norm -c 0.5623413208660099
+python linear.py -d cifar10 -m sela-v2 -c 1.0
+python linear.py -d cifar10 -m infomin -c 31.622778304361788
+python linear.py -d cifar10 -m byol -c 1.778279387127403
+python linear.py -d cifar10 -m deepcluster-v2 -c 0.3162277621819913
+python linear.py -d cifar10 -m swav -c 0.3162277621819913
+python linear.py -d cifar10 -m supervised -c 0.0562341298247638
+
+python linear.py -d cifar100 -m insdis -c 100.00000223517424
+python linear.py -d cifar100 -m moco-v1 -c 100000.00252621254
+python linear.py -d cifar100 -m pcl-v1 -c 56.23413185099295
+python linear.py -d cifar100 -m pirl -c 100.00000223517424
+python linear.py -d cifar100 -m pcl-v2 -c 9.99999985098839
+python linear.py -d cifar100 -m simclr-v1 --no-norm -c 0.3162277621819913
+python linear.py -d cifar100 -m moco-v2 -c 100.00000223517424
+python linear.py -d cifar100 -m simclr-v2 --no-norm -c 0.5623413208660099
+python linear.py -d cifar100 -m sela-v2 -c 1.778279387127403
+python linear.py -d cifar100 -m infomin -c 56.23413185099295
+python linear.py -d cifar100 -m byol -c 1.0
+python linear.py -d cifar100 -m deepcluster-v2 -c 0.3162277621819913
+python linear.py -d cifar100 -m swav -c 1.0
+python linear.py -d cifar100 -m supervised -c 0.0562341298247638
+
+python linear.py -d dtd -m insdis -c 562.3413185099295
+python linear.py -d dtd -m moco-v1 -c 316.2277690737784
+python linear.py -d dtd -m pcl-v1 -c 31.622778304361788
+python linear.py -d dtd -m pirl -c 316227.7712565657
+python linear.py -d dtd -m pcl-v2 -c 100.00000223517424
+python linear.py -d dtd -m simclr-v1 --no-norm -c 0.1778279462521979
+python linear.py -d dtd -m moco-v2 -c 1778.2794843157246
+python linear.py -d dtd -m simclr-v2 --no-norm -c 5.623413302903318
+python linear.py -d dtd -m sela-v2 -c 177.8279410688207
+python linear.py -d dtd -m infomin -c 56.23413185099295
+python linear.py -d dtd -m byol -c 1.778279387127403
+python linear.py -d dtd -m deepcluster-v2 -c 5.623413302903318
+python linear.py -d dtd -m swav -c 0.3162277621819913
+python linear.py -d dtd -m supervised -c 0.0177827946252197
+
+python linear.py -d flowers -m insdis -c 562341.3291514061
+python linear.py -d flowers -m moco-v1 -c 316227.7712565657
+python linear.py -d flowers -m pcl-v1 -c 100000.00252621254
+python linear.py -d flowers -m pirl -c 562341.3291514061
+python linear.py -d flowers -m pcl-v2 -c 562341.3291514061
+python linear.py -d flowers -m simclr-v1 --no-norm -c 1778.2794843157246
+python linear.py -d flowers -m moco-v2 -c 31622.77530666721
+python linear.py -d flowers -m simclr-v2 --no-norm -c 10000.000252621256
+python linear.py -d flowers -m sela-v2 -c 31622.77530666721
+python linear.py -d flowers -m infomin -c 316227.7712565657
+python linear.py -d flowers -m byol -c 56234.135072196805
+python linear.py -d flowers -m deepcluster-v2 -c 31622.77530666721
+python linear.py -d flowers -m swav -c 10000.000252621256
+python linear.py -d flowers -m supervised -c 999.999952502551
+
+python linear.py -d food -m insdis -c 177827.94843157247
+python linear.py -d food -m moco-v1 -c 56.23413185099295
+python linear.py -d food -m pcl-v1 -c 56234.135072196805
+python linear.py -d food -m pirl -c 56.23413185099295
+python linear.py -d food -m pcl-v2 -c 9.99999985098839
+python linear.py -d food -m simclr-v1 --no-norm -c 0.1778279462521979
+python linear.py -d food -m moco-v2 -c 177827.94843157247
+python linear.py -d food -m simclr-v2 --no-norm -c 0.3162277621819913
+python linear.py -d food -m sela-v2 -c 1.0
+python linear.py -d food -m infomin -c 56.23413185099295
+python linear.py -d food -m byol -c 0.5623413208660099
+python linear.py -d food -m deepcluster-v2 -c 0.3162277621819913
+python linear.py -d food -m swav -c 0.5623413208660099
+python linear.py -d food -m supervised -c 0.0562341298247638
+
+python linear.py -d pets -m insdis -c 31.622778304361788
+python linear.py -d pets -m moco-v1 -c 31622.77530666721
+python linear.py -d pets -m pcl-v1 -c 31622.77530666721
+python linear.py -d pets -m pirl -c 31622.77530666721
+python linear.py -d pets -m pcl-v2 -c 316.2277690737784
+python linear.py -d pets -m simclr-v1 --no-norm -c 17.782794106882072
+python linear.py -d pets -m moco-v2 -c 999.999952502551
+python linear.py -d pets -m simclr-v2 --no-norm -c 1.0
+python linear.py -d pets -m sela-v2 -c 177827.94843157247
+python linear.py -d pets -m infomin -c 562.3413185099295
+python linear.py -d pets -m byol -c 1.778279387127403
+python linear.py -d pets -m deepcluster-v2 -c 31622.77530666721
+python linear.py -d pets -m swav -c 17.782794106882072
+python linear.py -d pets -m supervised -c 562.3413185099295
+
+python linear.py -d sun397 -m insdis -c 3162.277748945447
+python linear.py -d sun397 -m moco-v1 -c 316.2277690737784
+python linear.py -d sun397 -m pcl-v1 -c 100.00000223517424
+python linear.py -d sun397 -m pirl -c 56.23413185099295
+python linear.py -d sun397 -m pcl-v2 -c 17.782794106882072
+python linear.py -d sun397 -m simclr-v1 --no-norm -c 0.1
+python linear.py -d sun397 -m moco-v2 -c 100.00000223517424
+python linear.py -d sun397 -m simclr-v2 --no-norm -c 0.3162277621819913
+python linear.py -d sun397 -m sela-v2 -c 1.778279387127403
+python linear.py -d sun397 -m infomin -c 56.23413185099295
+python linear.py -d sun397 -m byol -c 316.2277690737784
+python linear.py -d sun397 -m deepcluster-v2 -c 3.1622776814245577
+python linear.py -d sun397 -m swav -c 0.5623413208660099
+python linear.py -d sun397 -m supervised -c 0.0177827946252197
+
+python linear.py -d voc2007 -m insdis -c 17.782794106882072
+python linear.py -d voc2007 -m moco-v1 -c 31.622778304361788
+python linear.py -d voc2007 -m pcl-v1 -c 9.99999985098839
+python linear.py -d voc2007 -m pirl -c 31.622778304361788
+python linear.py -d voc2007 -m pcl-v2 -c 5.623413302903318
+python linear.py -d voc2007 -m simclr-v1 --no-norm -c 0.0562341298247638
+python linear.py -d voc2007 -m moco-v2 -c 9.99999985098839
+python linear.py -d voc2007 -m simclr-v2 --no-norm -c 0.1778279462521979
+python linear.py -d voc2007 -m sela-v2 -c 1.0
+python linear.py -d voc2007 -m infomin -c 9.99999985098839
+python linear.py -d voc2007 -m byol -c 0.3162277621819913
+python linear.py -d voc2007 -m deepcluster-v2 -c 0.3162277621819913
+python linear.py -d voc2007 -m swav -c 0.3162277621819913
+python linear.py -d voc2007 -m supervised -c 0.005623413223739
