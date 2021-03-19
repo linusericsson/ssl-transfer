3d7211eb37dd4375433d5976069f6d7dc604fe9a
Henry Gouk <henry.gouk@gmail.com>
2021-03-19 13:03:16 +0000

    Removed temporary scripts

--- semantic-segmentation/eval_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for d in "byol" "deepcluster-v2" "moco-v1" "moco-v2" "pirl" "sela-v2" "supervised" "swav"
-do
-	python eval_multipro.py --gpus 0,1 --cfg selfsupconfig/${d}.yaml | tee results/${d}.txt
-done
--- semantic-segmentation/run4-5.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# byol.pth  deepcluster-v2.pth  infomin.pth  insdis.pth  moco-v1.pth  moco-v2.pth  pcl-v1.pth  pcl-v2.pth  pirl.pth  sela-v2.pth	simclr-v1.pth  simclr-v2.pth  supervised.pth  supervised-simclr.pth  swav.pth
-
-export CUDA_VISIBLE_DEVICES=4,5
-#python train.py --gpus 0,1 --cfg selfsupconfig/pirl.yaml
-#python train.py --gpus 0,1 --cfg selfsupconfig/sela-v2.yaml
-python train.py --gpus 0,1 --cfg selfsupconfig/insdis.yaml
-
